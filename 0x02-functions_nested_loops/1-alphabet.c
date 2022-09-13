7f523f9926a9351fb465fd2be948f46a1518f020
Johnson <johnsondahmmylola@gmail.com>
2022-09-13 09:05:46 -0700

    1.alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
 /**
- * print_alphaber - function to print abc
+ *print_alphabet - function to print abc
  *
  * Return: 0
  */
-void print_alphaber(void)
+void print_alphabet(void)
 {
 	char c;
 
