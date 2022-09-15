d24589c31c6f8a7f28cf4acdc5fc6d0c507ba890
Johnson <johnsondahmmylola@gmail.com>
2022-09-15 12:00:54 -0700

    10-Triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,7 +3,7 @@
 
 /**
 *print_triangle - prints a triangle
-*@size:size parameter of triangle
+*@size: size parameter of triangle
 *Return: returns nothing
 */
 
@@ -22,7 +22,7 @@ void print_triangle(int size)
 
 			for (inc2 = 0; inc2 < inc1; inc2++)
 			{
-				putchar('#");
+				putchar('#');
 			}
 
 			if (inc1 == size)
