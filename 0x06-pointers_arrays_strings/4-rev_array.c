e33fd0374d216f454536c1a643b880e24c9e3c7b
Johnson <johnsondahmmylola@gmail.com>
2022-09-22 11:50:49 -0700

    4-rev_arr

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,12 +8,20 @@ include "main.h"
 
 void reverse_array(int *a, int n)
 {
-	int tmp, index;
+	int *p, i, aux, k;
 
-	for (index = n - 1; index > n / 2; index--)
+	p = a;
+
+	for (i = 1; i < n; i++)
 	{
-		tmp = a[n - 1 - index];
-		a[n - 1 - index] = a[index];
-		a[index] = tmp;
+		p++;
+	}
+
+	for (k = 0; k < i / 2; k++)
+	{
+		aux = a[k];
+		a[k] = *p;
+		*p = aux;
+		p--;
 	}
 }
