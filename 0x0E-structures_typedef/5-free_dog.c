2f90e6c8a1d927f89f28d84aeb9a8d3935b56d53
Johnson <johnsondahmmylola@gmail.com>
2022-10-10 08:00:31 -0700

    free_dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,26 +1,18 @@
-#ifndef _dog_h_
-#define _dog_h_
+#include <stdlib.h>
+#include "dog.h"
 
 /**
- * struct dog - such structure, wow
- * @name: dog' name
- * @age: dog's age
- * @owner: dog's owner
+ * free_dog - a function that frees dogs
+ * @d: pointer to dog struct
+ *
+ * Return: void
  */
 
-typedef struct dog
+void free_dog(dog_t *d)
 {
-	char *name;
-	float age;
-	char *owner;
-} dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
-void print_dog(struct dog *d);
-
-dog_t *new_dog(char *name, float age, char *owner);
-
-void free_dog(dog_t *d);
-
-#endif /* _dog_h_ */
+	if (d == 0)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
