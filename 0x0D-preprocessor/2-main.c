d9e2886a124fef6edc6008675705588730dcf354
Johnson <johnsondahmmylola@gmail.com>
2022-10-10 08:10:23 -0700

    2-fixed

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
-* main - prints name of file
+* main - prints the name of the file it was compiled from
+* followed by a new line
 *
 * Return: 0
 */
-
 int main(void)
 {
 	printf("%s\n" __FILE__);
